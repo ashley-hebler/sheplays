930be0d01784cbb2943b1a015115b007c65eac0d
Ashley Hebler <ashleyhebler@gmail.com>
2023-07-23 15:00:33 -0500

    Add lots of design updates
    - new logo
    - country flags
    - nav (still working on this)

--- src/config.ts
+++ src/config.ts
@@ -14,4 +14,42 @@ export const wnbaAPI = `${apiPrex}${leagueParams}${leagueMap.wnba}`;
 export const nwslAPI = `${apiPrex}${leagueParams}${leagueMap.nwsl}`;
 export const fifaAPI = `${apiPrex}${leagueParams}${leagueMap.fifa}`;
 export const auAPI = `${apiPrex}${leagueParams}${leagueMap.au}`;
-export const usaAPI = `${apiPrex}/games?&current=1&team_includes=usa`
+export const usaAPI = `${apiPrex}/games?&current=1&team_includes=usa`;
+
+export const countryCodes = {
+    "USA": "us",
+    "Australia": "au",
+    "Canada": "ca",
+    "China PR": "cn",
+    "France": "fr",
+    "Germany": "de",
+    "Italy": "it",
+    "Japan": "jp",
+    "Mexico": "mx",
+    "Netherlands": "nl",
+    "New Zealand": "nz",
+    "Norway": "no",
+    "Spain": "es",
+    "Sweden": "se",
+    "United Kingdom": "gb",
+    "England": "gb-eng",
+    "Thailand": "th",
+    "Brazil": "br",
+    "Argentina": "ar",
+    "Colombia": "co",
+    "Morocco": "ma",
+    "Panama": "pa",
+    "Korea Republic": "kr",
+    "Switzerland": "ch",
+    "Republic of Ireland": "ie",
+    "Denmark": "dk",
+    "Haiti": "ht",
+    "Jamaica": "jm",
+    "Philippines": "ph",
+    "Nigeria": "ng",
+    "Portugal": "pt",
+    "Zambia": "zm",
+    "Costa Rica": "cr",
+    "South Africa": "za",
+    
+}
