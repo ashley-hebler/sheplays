8f9004d62c32fa992470195f4e3f2bc76d4e5802
Ashley Hebler <ashleyhebler@gmail.com>
2024-04-10 21:59:32 -0500

    feat(template): :sparkles: add network pages

--- src/config.ts
+++ src/config.ts
@@ -34,6 +34,7 @@ export const teamsAPI = `${apiPrex}teams/?league=`;
 export const ncaaTeamsAPI = `${apiPrex}teams/?league=ncaa&limit=1000`;
 export const teamsLookupAPI = `${apiPrex}games/?current=1&team_includes=`;
 export const teamIdAPI = `${apiPrex}games/?current=1&team=`;
+export const networkIdAPI = `${apiPrex}games/?current=1&network=`;
 
 export const countryCodes = {
     "USA": "us",
