efe881b242ac240178fd8bce42e729af9bb99ee4
Ashley Hebler <ashleyhebler@gmail.com>
2025-01-25 13:34:22 -0600

    build(host): vercel -> netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,8 @@
 import { defineConfig } from 'astro/config';
-import AstroPWA from '@vite-pwa/astro';
-
-import vercel from "@astrojs/vercel/serverless";
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(
-    {
-      isr: {
-        // caches all pages on first request and saves for 8 hours
-        expiration: 60 * 60 * 8,
-      },
-    }
-  ),
-  integrations: [
-    AstroPWA({
-      manifest: {
-        name: "Sheplays.net",
-        short_name: "Sheplays.net",
-        description: "Women's sports times and broadcasts",
-        theme_color: "#011627ff",
-        icons: [
-          {
-            src: "/favicon.svg",
-            sizes: "any",
-            type: "image/svg+xml",
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [netlify()]
 });
